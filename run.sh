e50afd425d217054edc3f8f9d9b9b6f81a287c66
bergil <shavarynd@gmail.com>
2016-04-29 10:09:51 +0300

    fixed typo

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ behat() {
   docker run -ti --rm --link selenium_chrome -h docker-behat -v $(pwd):/home/behat/data:rw bergil/docker-behat /bin/bash -c "behat $*" ;
 }
 
-# Run Selenium container.
+# Run selenium container.
 docker run --restart=always --name=selenium_chrome -d -p 4444:4444 -v /dev/shm:/dev/shm selenium/standalone-chrome:2.52.0
 
 # Run behat container, initialize behat and run tests.
