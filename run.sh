26acb630dc4969a83c0ef3b42b501edc12dd9649
bergil <shavarynd@gmail.com>
2016-07-11 13:34:42 +0300

    updated reporter settings

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@
 
 # Create function behat () to run behat container with additional options.
 behat() {
-  docker run -ti --rm --link selenium_chrome  -h docker-behat -v $(pwd):/home/behat/data:rw bergil/docker-behat /bin/bash -c "behat $*";
+  docker run -ti --rm --link selenium_chrome -h docker-behat -v $(pwd):/home/behat/data:rw bergil/docker-behat /bin/bash -c "behat $*" ;
 }
 
 # Run selenium container.
 docker run --restart=always --name=selenium_chrome -d -p 4444:4444 -v /dev/shm:/dev/shm selenium/standalone-chrome:2.52.0
 
 # Run behat container, initialize behat and run tests.
-behat --init && behat -f cucumber_json && docker cp docker-behat:/home/behat/report.json report.json
+behat --init && behat --format=pretty --out=std --format=cucumber_json --out=std
 
 
 # Stop and remove selenium container.
