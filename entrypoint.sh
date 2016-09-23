add9817265664d1e49318c6f4fd8cb197b298cd0
Bergil <shavarynd@gmail.com>
2016-09-23 16:42:50 +0300

    Combined ci and basic images.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,36 @@
 #!/bin/sh
 
+if [ "$DOCKER_TESTING_ENVIRONMENT" -eq "1" ]; then
+
 # If else statement to keep behat container waiting while site is being built inside php container.
 # To run tests you need to pass some parameters to behat container.
 # E.x., docker-compose exec behat /srv/entrypoint.sh "--format=pretty --out=std --format=cucumber_json --out=std".
-if [ -z "$*" ]; then
+  if [ -z "$*" ]; then
 
-  # Foreground command to keep container working.
-  tail -f /dev/null
-else
+    # Foreground command to keep container working.
+    tail -f /dev/null
+  else
 
-  # Run Behat with parameters passed using CMD.
-  bin/behat $*
-
-  # Fix permissions to artifacts folder.
-  chmod -R 777 /srv/artifacts
+    # Run Behat with parameters passed using CMD.
+    bin/behat $*
+  fi
 fi
+
+if [ "$DOCKER_TESTING_ENVIRONMENT" -eq "0" ]; then
+
+  # Give some additional time for Selenium server to start properly.
+  sleep 2
+
+  if [ -z "$*" ]; then
+
+    # Set default parameters for behat if no parameters was passed.
+    bin/behat --format=pretty --out=std --format=cucumber_json --out=std
+  else
+
+    # Run Behat with parameters passed using CMD.
+    bin/behat $*
+  fi
+fi
+
+# Fix permissions to artifacts folder.
+chmod -R 777 /srv/artifacts
