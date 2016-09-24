86bfe53ca3a67d76fc05f0008e543e3e112b09ef
Dmytro Shavaryn <shavarynd@gmail.com>
2016-09-24 17:19:27 +0300

    Refactored entrypoint.sh script.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,35 +1,30 @@
 #!/bin/sh
 
-# Section for running tests in Docker environments.
-if [ "$DOCKER_TESTING_ENVIRONMENT" -eq "1" ]; then
+# Section to keep container working in detached mode.
+if [ -z "$*" ]; then
 
-# If else statement to keep Behat container waiting while site is being built inside php container.
-# To run tests you need to pass some parameters to Behat container.
-# E.x., docker-compose exec behat /srv/entrypoint.sh "--format=pretty --out=std --format=cucumber_json --out=std".
-  if [ -z "$*" ]; then
+  # Update composer if $DOCKER_TESTING_ENVIRONMENT = 1 and $COMPOSER_UPDATE = 1.
+  if [ "$DOCKER_TESTING_ENVIRONMENT" -eq "1" ] && [ "$COMPOSER_UPDATE" -eq "1" ]; then
+    composer update
+  fi
 
-    # Update composer if $COMPOSER_UPDATE = 1.
-    if [ "$COMPOSER_UPDATE" -eq "1" ]; then
-        composer update
-    fi
+  # Foreground command to keep container working.
+  tail -f /dev/null
 
-    # Foreground command to keep container working.
-    tail -f /dev/null
+# Section to run tests.
+else
 
-  # Run Behat with parameters passed using CMD.
-  else
+  # Section for running tests in Docker environments.
+  if [ "$DOCKER_TESTING_ENVIRONMENT" -eq "1" ]; then
+
+    # To run tests you need to pass some parameters to Behat container.
+    # E.x., docker-compose exec behat /srv/entrypoint.sh "--format=pretty --out=std --format=cucumber_json --out=std".
+    # Run Behat with parameters passed using CMD.
     bin/behat $*
   fi
-fi
 
-# Section for running tests in non-Docker environments.
-if [ "$DOCKER_TESTING_ENVIRONMENT" -eq "0" ]; then
-
-  if [ -z "$*" ]; then
-
-    # Foreground command to keep container working.
-    tail -f /dev/null
-  else
+  # Section for running tests in non-Docker environments.
+  if [ "$DOCKER_TESTING_ENVIRONMENT" -eq "0" ]; then
 
     # Update composer if $COMPOSER_UPDATE = 1.
     if [ "$COMPOSER_UPDATE" -eq "1" ]; then
@@ -42,8 +37,8 @@ if [ "$DOCKER_TESTING_ENVIRONMENT" -eq "0" ]; then
 
     # Run Behat with passed parameters.
     bin/behat $*
-
-    # Fix permissions to artifacts folder.
-    chmod -R 777 /srv/artifacts
   fi
+
+  # Fix permissions to artifacts folder.
+  chmod -R 777 /srv/artifacts
 fi
