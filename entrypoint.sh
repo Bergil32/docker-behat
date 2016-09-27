8484008812db6e9e429dc8d702ef7980ed00499f
Bergil <shavarynd@gmail.com>
2016-09-27 15:04:14 +0300

    Added listener that checks if composer update process was completed.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,22 +3,30 @@
 # Section to keep container working in detached mode.
 if [ -z "$*" ]; then
 
-  # Update composer if $DOCKER_TESTING_ENVIRONMENT = 1 and $COMPOSER_UPDATE = 1.
+  # Update Composer if $DOCKER_TESTING_ENVIRONMENT = 1 and $COMPOSER_UPDATE = 1.
   if [ "$DOCKER_TESTING_ENVIRONMENT" -eq "1" ] && [ "$COMPOSER_UPDATE" -eq "1" ]; then
-    composer update
+    # Update Composer and write "1" to tmp file after update finished.
+    rm -f /tmp/update_finished && composer update && echo "1" >> /tmp/update_finished
   fi
 
   # Foreground command to keep container working.
   tail -f /dev/null
 
 # Section to run tests.
+# To run tests you need to pass some parameters to Behat container.
+# E.x., docker-compose exec behat /srv/entrypoint.sh "--format=pretty --out=std --format=cucumber_json --out=std".
 else
 
   # Section for running tests in Docker environments.
   if [ "$DOCKER_TESTING_ENVIRONMENT" -eq "1" ]; then
 
-    # To run tests you need to pass some parameters to Behat container.
-    # E.x., docker-compose exec behat /srv/entrypoint.sh "--format=pretty --out=std --format=cucumber_json --out=std".
+    # Pause until Composer update will be finished.
+    if [ "$COMPOSER_UPDATE" -eq "1" ]; then
+      while [  ! -e "/tmp/update_finished" ]; do
+        sleep 1
+      done
+    fi
+
     # Run Behat with parameters passed using CMD.
     bin/behat $*
   fi
